43d4f2e375738519080968471632aa15ad94b20d
rms1990 <raphaelsabino90@gmail.com>
2020-10-08 23:11:17 -0300

    adicao do programa operacao e teste

--- com/Raphael/test/test_operacoes.py
+++ com/Raphael/test/test_operacoes.py
@@ -1,3 +1,7 @@
-def test_answer():
-    assert armstrong(370)
+from com.Raphael import operacoes
 
+def test_answer():
+    assert operacoes.armstrong(370)
+
+def test_answer2():
+    assert operacoes.armstrong(371)
