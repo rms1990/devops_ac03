9a22a0a161394f277142013f654af5aa7ddfe192
rms1990 <raphaelsabino90@gmail.com>
2020-10-08 22:58:09 -0300

    adicao de arquivo para test travis

--- com/Raphael/test/test_operacoes.py
+++ com/Raphael/test/test_operacoes.py
@@ -1,7 +1,12 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-
+def armstrong(n):
+  soma = 0  
+  temp = n  
+  while temp > 0:  
+    digito = temp % 10  
+    soma += digito ** 3  
+    temp //= 10  
+  return n == soma
 
 def test_answer():
-    assert func(3) == 5
+    assert armstrong(370)
+
