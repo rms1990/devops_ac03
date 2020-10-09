ed1cc64d300631e756f678fc8e8eb878ab88cecc
rms1990 <69654735+rms1990@users.noreply.github.com>
2020-10-08 23:00:32 -0300

    Update test_operacoes.py

--- com/Raphael/test/test_operacoes.py
+++ com/Raphael/test/test_operacoes.py
@@ -1,12 +1,3 @@
-def armstrong(n):
-  soma = 0  
-  temp = n  
-  while temp > 0:  
-    digito = temp % 10  
-    soma += digito ** 3  
-    temp //= 10  
-  return n == soma
-
 def test_answer():
     assert armstrong(370)
 
